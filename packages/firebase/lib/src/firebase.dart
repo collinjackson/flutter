3856353c2fe0b3fb3fbc9a9835d22871d9ce9c44
Collin Jackson <jackson@google.com>
2016-01-08 17:52:39 -0800

    Beginnings of on/off implementation

--- packages/firebase/lib/src/firebase.dart
+++ packages/firebase/lib/src/firebase.dart
@@ -9,9 +9,17 @@ import 'package:sky_services/firebase/firebase.mojom.dart' as mojo;
 
 export 'package:sky_services/firebase/firebase.mojom.dart' show DataSnapshot, EventType;
 
+abstract class ValueEventListener {
+  void onCancelled(mojo.Error error);
+  void onDataChange(mojo.DataSnapshot snapshot);
+}
+
 class Firebase {
 
   mojo.FirebaseProxy _firebase;
+
+  Map<ValueEventListener, FirebaseHandle> _valueEventListeners =
+    new Map<ValueEventListener, FirebaseHandleProxy>();
 
   Firebase(String url) : _firebase = new mojo.FirebaseProxy.unbound() {
     shell.connectToService("firebase::Firebase", _firebase);
@@ -30,6 +38,20 @@ class Firebase {
     mojo.FirebaseProxy proxy = new mojo.FirebaseProxy.unbound();
     _firebase.ptr.getChild(path, proxy);
     return new Firebase._withProxy(proxy);
+  }
+
+  ValueEventListener on(mojo.EventType eventType, ValueEventListener listener) {
+    mojo.FirebaseHandleProxy proxy = new mojo.FirebaseHandleProxy.unbound();
+    mojo.ValueEventListenerStub stub = new mojo.ValueEventListenerStub.unbound()
+      ..impl = listener;
+    _valueEventListeners[listener] = proxy;
+    _firebase.ptr.addValueEventListener(eventType, stub, proxy);
+    return listener;
+  }
+
+  void off(mojo.EventType eventType, ValueEventListener listener) async {
+    mojo.FirebaseHandleProxy proxy = _valueEventListeners[listener];
+    _firebase.ptr.removeEventListener(eventType, proxy.ptr);
   }
 
   Future<mojo.DataSnapshot> once(mojo.EventType eventType) async {
